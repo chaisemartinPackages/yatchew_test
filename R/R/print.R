ef5ff29296db9a884bff065d7cdb1bcb9d0c11a7
Diego Ciccia <diegociccia@tiscali.it>
2024-04-17 14:01:01 +0200

    Update print method

--- R/R/print.R
+++ R/R/print.R
@@ -2,33 +2,28 @@
 #' @name print.yatchew_test
 #' @description A customized printed display for yatchew_test output
 #' @param x A yatchew_test object
-#' @param suppress_intro The description of the test is not printed
 #' @param ... Undocumented
 #' @returns No return, custom print method for yatchew_test objects.
 #' @export
 #' @noRd
-print.yatchew_test <- function(x, suppress_intro = FALSE, ...) {
+print.yatchew_test <- function(x, ...) {
     clim <- ncol(x$results)
-    setting <- ifelse(x$args$het_robust, "- Heteroskedasticity-robust Test", "- Test under homoskedasticity")
-    cit <- ifelse(x$args$het_robust, ", robust version of de Chaisemartin & D'Haultfoeuille (2024)", "")
+    setting <- ifelse(isTRUE(x$args$het_robust), "- Heteroskedasticity-robust Test", "- Test under homoskedasticity")
+    cit <- ifelse(isTRUE(x$args$het_robust), ", robust version of de Chaisemartin & D'Haultfoeuille (2024)", "")
     type <- ""
     if (length(x$args$D) > 1) {
         cat("\n")
         type <- "multivariate "
     }
 
-    if (!suppress_intro) {
-        cat(sprintf(" Yatchew (1997) %stest%s\n", type, cit))
-    }
+    cat(sprintf("\n Yatchew (1997) %stest%s\n", type, cit))
     dis_mat <- matrix(NA, nrow = 1, ncol = clim)
     dis_mat[1, 1:(clim-1)] <- sprintf("%.4f", x$results[1,1:(clim-1)])
     dis_mat[1, clim] <- sprintf("%.0f", x$results[1,clim])
     colnames(dis_mat) <- colnames(x$results)
-    rownames(dis_mat) <- rownames(x$results)
+    rownames(dis_mat) <- ""
     print(noquote(dis_mat), drop = FALSE)
-    if (!suppress_intro) {
-        cat(sprintf("\n H0: E[Y|D] is linear in D %s\n", setting))
-    }
+    cat(sprintf(" H0: E[Y|D] is linear in D %s\n", setting))
     cat("\n")
 }
 
